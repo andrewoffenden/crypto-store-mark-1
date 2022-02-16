3692ebbd4cbce025f4baaaaa4e415981658d09d0
Andrew Woffenden <andrewoffenden@gmail.com>
2022-02-15 16:19:52 -0800

    Added packages to integrate web3.  Added basic controls to page to connect to metamask.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import Web3 from "web3"
+import detectEthereumProvider from '@metamask/detect-provider';
+import "./css/App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
+
+
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+
+  const [account, setAccount] = useState(null)
+
+  useEffect(() => {
+    const loadProvider = async () =>{
+      //npm package detects the provider
+      const provider = await detectEthereumProvider()
+
+      if (provider) {
+        //request metamask/provider to connect account
+        // provider.request({method: "eth_requestAccounts"})
+
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        })
+      } else {
+        console.error("Please install metamask")
+      }
+    };
+
+    loadProvider()
+  }, []);
+
+  useEffect(() => {
+    const getAccounts = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+
+      //uses web3 api instantiated above in load provider in order to get accounts array
+      setAccount(accounts[0])
+    }
+
+    //executed only if you have a web3 api
+    web3Api.web3 && getAccounts()
+    //executed every time web3 is assigned
+  }, [web3Api.web3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="application-wrapper">
+    <div className="connect-account-controls">
+        <div className="is-flex is-align-items-center">
+          { account ?
+            <>
+              <span>
+                <strong className="mr-2">Account:</strong>
+              </span>
+              <div>
+                {account}
+              </div>
+            </>
+            :
+            <button
+              className="button is-info"
+              onClick={() =>
+                web3Api.provider.request({method: "eth_requestAccounts"}
+              )}
+            >
+              Connect Metamask
+            </button>
+          }
+        </div>
+        <div className="balance-view is-size-2 my-4">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './css/index.css';
+import  'bulma/css/bulma.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
